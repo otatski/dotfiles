c230c5d676d999a41e596e58af55d24547146179
papa0four <papa.o.deuce@gmail.com>
2022-08-11 13:32:15 -0400

    added colorization to update outputs

--- .nvim_config/update_local.sh
+++ .nvim_config/update_local.sh
@@ -7,30 +7,29 @@ NC='\033[0m'
 matstrovim=$HOME/dotfiles/.nvim_config/user
 astrovim=$HOME/dotfiles/nvim/.config/nvim/lua/user
 
-if [ $astrovim -ot $matstrovim ]; then
+if [ $astrovim -nt $matstrovim ]; then
     echo "You are about to replace a newer version with an older version.";
     echo "Are you sure? [y/N]";
     read response
     if [ $response == 'y' ] || [ $response == 'Y' ]; then
-        rm -rf $matstrovim;
-        cp -r $astrovim $matstrovim;
-        echo -e "${RED}deleted:${NC} $matstrovim";
-        echo -e "${GREEN}replaced with:${NC} $astrovim";
-        exit 0;    
+        rm -rf $astrovim;
+        cp -r $matstrovim $astrovim;
+        echo -e "${RED}deleted:${NC} $astrovim";
+        echo -e "${GREEN}replaced with:${NC} $matstrovim";
+        exit 0;
     elif [ $response == 'n' ] || [ $response == 'N' ]; then
         exit 0;
     else
         echo "Invalid response, exiting ...";
         exit 1;
     fi
-elif [ $astrovim -nt $matstrovim ]; then
-    rm -rf $matstrovim;
-    cp -r $astrovim $matstrovim;
-    echo -e "${RED}deleted:${NC} $matstrovim";         
-    echo -e "${GREEN}replaced with:${NC} $astrovim";
+elif [ $astrovim -ot $matstrovim ]; then
+    rm -rf $astrovim;
+    cp -r $matstrovim $astrovim;
+    echo -e "${RED}deleted${NC}: $astrovim";
+    echo -e "${GREEN}replaced with:${NC} $matstrovim";
     exit 0;
 else
     echo "Nothing to update ...";
     exit 0;
 fi
-
